ce2c31906569ad0b812cb946ecf0bbb838a26352
Nikita Adodin <tremsn@yandex.ru>
2022-03-30 23:21:30 +0300

    Logic for num == 0

--- Ex1.py
+++ Ex1.py
@@ -1,6 +1,8 @@
 print("Четное число или нечетное\n")
 num = int(input("Введите число: "))
-if num % 2 == 0:
+if num == 0:
+    print(num, " - это ноль")
+elif num % 2 == 0:
     print(num, " - четное число")
 elif num % 2 == 1:
     print(num, " - нечетное число")
